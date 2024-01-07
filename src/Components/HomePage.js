5bceba542bde4cb1e5b90ec0dabdf1780eba106d
chaitanyaGosavi <97080993+chaitanyaGosavi@users.noreply.github.com>
2024-01-07 23:47:14 +0530

    Update HomePage.js

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -9,7 +9,7 @@ const HomePage = () => {
       <div>
         <Banner />
         <CoinsTable />
-        <Typography variant='h7' style={{textAlign:'right', marginTop:20, marginRight:5}}>
+        <Typography variant='h7' style={{textAlign:'center', marginTop:20, marginRight:5}}>
                 <a href="https://www.freepik.com/free-vector/gradient-technology-background-with-lights_18981336.htm#page=5&query=website%20background&position=49&from_view=keyword&track=ais&uuid=b2e1f670-cccf-480c-a92b-92331ae8fd5b">
                     Image by pikisuperstar 
                 </a>
